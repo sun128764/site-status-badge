6bc6443074ef5983d24c72ecab8579847c38f880
sun128764 <sun128764@gmail.com>
2025-12-24 01:06:19 +0800

    feat real UA

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function checkSiteStatus(url: string): Promise<boolean> {
 			method: "HEAD",
 			signal: controller.signal,
 			headers: {
-				"User-Agent": "Site-Status-Badge/1.0",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
 			},
 		});
 
