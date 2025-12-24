4194c4d167638b8a0007b07a526b618a34ce695d
sun128764 <sun128764@gmail.com>
2025-12-24 11:41:24 +0800

    feat use rss link

--- src/sites.ts
+++ src/sites.ts
@@ -3,12 +3,12 @@
  * key: URL 路径中使用的标识符
  * value: 需要检测的网站 URL
  */
-export const SITES:  Record<string, string> = {
-  vcbs:"https://vcb-s.com/",
-  dmhy: "https://dmhy.org/",
-  bangumi: "https://bangumi.moe/",
-  nyaa: "https://nyaa.si/",
-  acgrip: "https://acg.rip/",
-  acgnx:"https://share.acgnx.se/",
-  sacgnx:"https://www.acgnx.se/"
+export const SITES: Record<string, string> = {
+  vcbs: "https://vcb-s.com/",
+  dmhy: "https://dmhy.org/topics/rss/rss.xml",
+  bangumi: "https://bangumi.moe/rss/latest",
+  nyaa: "https://nyaa.si/?page=rss",
+  acgrip: "https://acg.rip/.xml",
+  acgnx: "https://share.acgnx.se/rss.xml",
+  sacgnx: "https://www.acgnx.se/rss.xml"
 };
